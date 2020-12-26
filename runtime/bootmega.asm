ad4403062511169e010f6be325ddc1ac7babd361
Paul Robson <paul@robsons.org.uk>
2020-12-26 21:36:59 +0000

    Update

--- runtime/bootmega.asm
+++ runtime/bootmega.asm
@@ -9,40 +9,44 @@
 ; ***************************************************************************************************************
 ; ***************************************************************************************************************
 
-myTemp4 = $02
+myTemp4 = $02                                   ; temp far pointer
+
+zeroPageStart = $10                             ; zero page allocation here.
+
+variableMax = 32 								; how many variables allowed in test version
 
 ; ***************************************************************************************************************
 ;
 ;                                               Header, a BASIC line
 ;
 ; ***************************************************************************************************************
-
-        *=$2001
-        !word   endLine                         ; link to next line
-        !word   1                               ; line number
-        !byte   $fe,$02                         ; BANK
-        !text   "0:"                            ; 0:
-        !byte   $9e                             ; SYS
-        !text   "8224"                          ; 8224 (e.g. $2020)
-        !byte   0                               ; end of line
-        !word   0                               ; end of program.
+						
+	*=$2001
+	!word   endLine                         ; link to next line
+	!word   1                               ; line number
+	!byte   $fe,$02                         ; BANK
+	!text   "0:"                            ; 0:
+	!byte   $9e                             ; SYS
+	!text   "8224"                          ; 8224 (e.g. $2020)
+	!byte   0                               ; end of line
+	!word   0                               ; end of program.
 endLine   
 
-        *=$2020
-        sei                                     ; disable Interrupts
-        lda     #$35                            ; C64 RAM visibility/tape off. 
-        sta     $01
+	*=$2020
+	sei                                     ; disable Interrupts
+	lda     #$35                            ; C64 RAM visibility/tape off. 
+	sta     $01
 
-        lda     #$00                            ; Reset mapping - lower/upper offsets = 0
-        tax                                     ; so everything except $Dxxx RAM ?
-        tay 
-        taz 
-        map
-        eom
+	lda     #$00                            ; Reset mapping - lower/upper offsets = 0
+	tax                                     ; so everything except $Dxxx RAM ?
+	tay 
+	taz 
+	map
+	eom
 
-        lda     #$FE                            ; use page $FExx as ZP.
-        tab
-        jmp     start
+	lda     #$FE                            ; use page $FExx as ZP.
+	tab
+	jmp     start
 
 ; ***************************************************************************************************************
 ;
@@ -51,30 +55,30 @@ endLine
 ; ***************************************************************************************************************
 
 PrintCharacter
-        pha
-        phx
-        phz
+	pha
+	phx
+	phz
 
-        ldx     charPos                         ; Z X offset into first 256 chars
-        ldz     charPos
-        inc     charPos
-        and     #63                             ; handle PETSCII
-        sta     $0800,x
+	ldx     charPos                         ; Z X offset into first 256 chars
+	ldz     charPos
+	inc     charPos
+	and     #63                             ; handle PETSCII
+	sta     $0800,x
 
-        lda     #$00                            ; write colour to $01F800 (Color RAM)
-        sta     myTemp4
-        lda     #$F8
-        sta     myTemp4+1
-        lda     #$01
-        sta     myTemp4+2
-        lda     #$00
-        sta     myTemp4+3
-        lda     #$01
-        sta     [myTemp4],z
-        plz
-        plx
-        pla
-        rts
+	lda     #$00                            ; write colour to $01F800 (Color RAM)
+	sta     myTemp4
+	lda     #$F8
+	sta     myTemp4+1
+	lda     #$01
+	sta     myTemp4+2
+	lda     #$00
+	sta     myTemp4+3
+	lda     #$01
+	sta     [myTemp4],z
+	plz
+	plx
+	pla
+	rts
 
 charPos
-        !byte   0
+	!byte   0
--- runtime/main.asm
+++ runtime/main.asm
@@ -9,44 +9,112 @@
 ; ***************************************************************************************************************
 ; ***************************************************************************************************************
 
-    !if target=1 {
-    !source "bootmega.asm"
-    } else {
-    !source "boottest.asm"
-    }
+	!if target=1 {
+	!source "bootmega.asm"
+	} 
+	!if target=2 {
+	!source "boottest.asm"
+	}
+
+	!src 	"data.asm"
+
+; ***************************************************************************************************************
+;
+;					Runtime Header starts here (if not run in test handler, target #0)
+;
+; ***************************************************************************************************************
 
 start
-        ldx     #0
-        lda     #42
-        sta     $FFC2
+		jmp 	runApplication 				; +0  is jump to initial code
+		!byte 	0
+		jmp 	$0000 						; +4  is opcode execution routine
+		!byte 	0
+		!word	$0000 						; +8  is the address of the first procedure.
+		!word 	start  						; +10 is the load address
+		!word 	UnnitialisedVariables 		; +12 is the start of the uninitialised variables.
+		!word 	EndVariableSpace 			; +14 is the end of the uninitialised variables.
+
+; ***************************************************************************************************************
+;
+;									Runtime code starts here
+;
+; ***************************************************************************************************************
+
+		* = start+64
+
+runApplication		
+		ldx 	#$FF
+		txs
+		+set16 	pctr,1042
+		+inc16  pctr
+		ldx     #0
+		lda     #42
+		sta     $FFC2 
 loop    lda     $2020,x
-        jsr     PrintHex
-        lda     #' '
-        jsr     PrintCharacter
-        inx
-        cpx     #32
-        bne     loop        
+		jsr     PrintHexSpace
+		inx
+		cpx     #32
+		bne     loop        
 halt
-        jmp     halt
+		jmp     halt
 
+; ***************************************************************************************************************
+;
+;						PrintA as hex constant with/without leading space
+;
+; ***************************************************************************************************************
+
+PrintHexSpace:
+		pha
+		lda 	#' '
+		jsr 	PrintCharacter
+		pla
 PrintHex:        
-        pha
-        lsr     
-        lsr     
-        lsr     
-        lsr     
-        jsr     _PrintNibble
-        pla
+		pha
+		lsr     
+		lsr     
+		lsr     
+		lsr     
+		jsr     _PrintNibble
+		pla
 _PrintNibble:
-        pha
-        and     #15
-        cmp     #10
-        bcc     +
-        adc     #6
+		pha
+		and     #15
+		cmp     #10
+		bcc     +
+		adc     #6
 +
-        adc     #48
-        jsr     PrintCharacter
-        pla 
-        rts
+		adc     #48
+		jsr     PrintCharacter
+		pla 
+		rts
 
+; ***************************************************************************************************************
+;
+;						Testing code goes here (this is for non runtime builds)
+;
+; ***************************************************************************************************************
 
+CodeSpace:
+
+; ***************************************************************************************************************
+;
+;					Then the variables with predefined values, constants, addresses etc.
+;
+; ***************************************************************************************************************
+
+		!align 	255,0 						; put on page boundary.
+SystemVariables:
+
+; ***************************************************************************************************************
+;
+;									Then the uninitialised variables
+;
+; ***************************************************************************************************************
+
+UnnitialisedVariables:
+		!if target > 0 { 					; allocate memory if not runtime build.
+			!fill 	variableMax * 2,$FF
+		}
+
+EndVariableSpace:
